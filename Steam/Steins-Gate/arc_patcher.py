cade5def269934e19f66f7fed0be36935d245c82
wmltogether <wmltogether@gmail.com>
2016-09-12 11:38:17 +0800

    Update patcher

--- Steam/Steins-Gate/arc_patcher.py
+++ Steam/Steins-Gate/arc_patcher.py
@@ -4,7 +4,7 @@ import codecs
 from arc_tool import *
 
 arc = ARC("script.mpk")
-arc.patch_arc("script.mpk",'patch')
+arc.patch_arc("script.mpk",'patch//')
 
 arc = ARC("system.mpk")
-arc.patch_arc("script.mpk", 'patch')
+arc.patch_arc("system.mpk", 'patch//')
